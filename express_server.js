b7ebffe4a0e21786802ebf9bc780abfb6c0641f2
tessfbs <tessfbs@gmail.com>
2023-02-19 22:47:25 -0500

    added response for /hello

--- express_server.js
+++ express_server.js
@@ -14,6 +14,14 @@ app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/hello", (req, res) => {
+  res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
 //Start the server by calling the app.listen() method, passing in the PORT constant as the first argument and a callback function that logs a message to the console when the server is ready to receive requests:
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
