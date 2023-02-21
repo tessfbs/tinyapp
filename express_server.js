7513d6f9516ba178cbe8434788d13ca0b7f20b16
tessfbs <tessfbs@gmail.com>
2023-02-21 16:07:30 -0500

    edit function added

--- express_server.js
+++ express_server.js
@@ -4,11 +4,17 @@ const app = express(); //creates a new Express application that can be used to h
 
 const PORT = 8080; // default port 8080, which holds the port number on which the server will listen
 
+app.set("view engine", "ejs"); //tells the Express app to use EJS as its templating engine
+
 //Define an object urlDatabase which maps short URLs to their corresponding full URLs
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+//tells the Express app to parse incoming requests with urlencoded payloads and expose the resulting object on req.body.
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -21,6 +27,64 @@ app.get("/urls.json", (req, res) => {
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+
+app.get("/urls", (req,res) => { 
+  const templateVars = {urls: urlDatabase};
+  res.render("urls_index",templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/urls/:id", (req, res) => {
+  console.log("hello")
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
+function generateRandomString() {
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  let result = '';
+  for (let i = 0; i < 6; i++) {
+    result += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return result;
+}
+//response when the form is submitted
+app.post("/urls", (req, res) => {
+  const randomString = generateRandomString()
+  urlDatabase[randomString] = req.body.longURL;
+  // console.log(urlDatabase)
+  // console.log(req.body); // Log the POST request body to the console
+  res.redirect(`/urls/${randomString}`)
+  // res.status(200).send("ok"); // Respond with 'Ok' (we will replace this)
+});
+
+//Redirect any request to "/u/:id" to its longURL
+app.get("/u/:id", (req, res) => {
+  console.log(urlDatabase[req.params.id])
+  const longURL = urlDatabase[req.params.id];
+  res.redirect(longURL);
+});
+
+//delete an url
+app.post("/urls/:id/delete", (req, res) => {
+  delete urlDatabase[req.params.id]
+  // console.log(req.params.id)
+  res.redirect(`/urls`)
+})
+
+//update URL
+app.post("/u/:id/update", (req, res) => {
+  const newUrl = req.body.newUrl
+  urlDatabase[req.params.id] = newUrl;
+  res.redirect(`/urls/${req.params.id}`)
+})
+
+
+
+
 
 //Start the server by calling the app.listen() method, passing in the PORT constant as the first argument and a callback function that logs a message to the console when the server is ready to receive requests:
 app.listen(PORT, () => {
